b93b102645a8f07245becc3a98a3c440e73958f8
Pabloo17 <pabgar12@ucm.es>
2018-11-25 19:30:08 +0100

    añadido campo status a los datos y muestra el circulo del color que corresponde

--- plantilla/web/js/pruebaVMS.js
+++ plantilla/web/js/pruebaVMS.js
@@ -6,6 +6,7 @@ let data = {
             name: "Linux1",
             ram: 2048,
             hdd: 20480,
+            status: "running",
             cpu: 100,
             cores: 1
         },
@@ -13,6 +14,7 @@ let data = {
             name: "Linux2",
             ram: 2048,
             hdd: 20480,
+            status: "suspended",
             cpu: 100,
             cores: 1
         },
@@ -20,6 +22,7 @@ let data = {
             name: "Linux3",
             ram: 2048,
             hdd: 20480,
+            status: "off",
             cpu: 100,
             cores: 1
         },
@@ -32,12 +35,27 @@ let data = {
     ]
 }
 
-function crearLista(elem) {
-    return "<li class= 'list-group-item list-vms'> <input type='checkbox' ></input>" + elem +  "</li>";
+
+function functionCheck(){
+
+}
+
+//index se supone que es el id que le asignamos dinamicamente a cada fila de la tabla pero todavia no se como hacerlo
+function crearLista(name, status, index) {
+    if (status == "running") {
+        return "<li class= 'list-group-item list-vms'> <input type='checkbox' onclick='functionCheck()' id="+index+"></input>" + name + "<span class='badge badge-success mt-1'> </span> </li>";
+    }
+    else if (status == "suspended"){
+        return "<li class= 'list-group-item list-vms'> <input type='checkbox' onclick='functionCheck()' id="+index+"></input>" + name + "<span class='badge badge-warning mt-1'> </span> </li>";
+    }
+    else{
+        return "<li class= 'list-group-item list-vms'> <input type='checkbox' onclick='functionCheck()' id="+index+"></input>" + name + "<span class='badge badge-danger mt-1'> </span> </li>";
+    }
+
 }
 $("#replace").click(e => {
     $("#listaVM").empty();
     data.vms.forEach(
-        vm => $("#listaVM").append(crearLista(vm.name))
+        vm => $("#listaVM").append(crearLista(vm.name, vm.status))
     )
 })
